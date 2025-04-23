cafab90836829f6d163e6b460fc48420e1a0bc92
ShockieToky <aymerick.guerinet@laposte.net>
2025-04-23 14:41:53 +0200

    Add migration files for cards, card types, types, and sets tables

---
+++ server/database/migrations/2025_04_23_120006_create_cards_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('number');
+            $table->foreignId('set_id')->constrained()->cascadeOnDelete();
+            $table->string('rarity');
+            $table->string('nationalPokedexNumbers')->nullable();
+            $table->string('images_small');
+            $table->string('images_large');
+            $table->date('obtained_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
